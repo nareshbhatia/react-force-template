fd219c6704c300c6ddd78e4375c198717a8a1ea9
Naresh Bhatia <naresh.archfirst@gmail.com>
2019-06-10 22:58:31 -0400

    Moved Material-UI ThemeProvider into App.tsx
    This allows to pull in dynamic themes from stores that generally exist only at the App level, not in index.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React, { Suspense } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
 import { ErrorBoundary, Loading } from '@nareshbhatia/react-force';
+import { theme } from './components';
 import { HomePage } from './pages';
 
 export const App: React.FC = () => {
     return (
         <ErrorBoundary>
             <Suspense fallback={<Loading />}>
-                <HomePage />
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <HomePage />
+                </ThemeProvider>
             </Suspense>
         </ErrorBoundary>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/styles';
 import { App } from './App';
-import { theme } from './components';
 
-ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-    </ThemeProvider>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
